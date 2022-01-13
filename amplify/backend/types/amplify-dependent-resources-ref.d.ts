ed5d65a5603544d42161c7c48ac5e60500a425df
Leena Kamble <leenak@godrej.in>
2022-01-13 17:20:21 +0530

    Auth Added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "employeeportala35b739c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -12,23 +12,33 @@ import EmployeeForm from './Components/EmployeeForm';
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import {loadEmployees} from './actions/action-creator'
+import { loadEmployees } from './actions/action-creator'
 
-function App({loadEmployees}) {
+//amplify packages
+import { Amplify } from 'aws-amplify'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
+import awsEXports from './aws-exports';
+Amplify.configure(awsEXports);
+
+function App({ loadEmployees }) {
   loadEmployees();
   return (
+    <Authenticator loginMechanisms={['username']}>
+    {({signOut,user})=>(
     <Router>
-
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand as={Link} to="/">{process.env.REACT_APP_APPLICATION_NAME}</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          </Nav>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+              <Nav.Link onCLick={signOut}>SignOut</Nav.Link>
+              <Nav.Link> Welocme {user.username}</Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
@@ -37,19 +47,21 @@ function App({loadEmployees}) {
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/about" element={<About />}></Route>
           <Route exact path="/contact" element={<Contact />}></Route>
-          <Route exact path="/employees/loc/:locId/empCode/:empCode" element={<EmployeeDetail/>}></Route>
-          <Route exact path="/employee/create" element={<EmployeeForm/>}></Route>
+          <Route exact path="/employees/loc/:locId/empCode/:empCode" element={<EmployeeDetail />}></Route>
+          <Route exact path="/employee/create" element={<EmployeeForm />}></Route>
         </Routes>
       </div>
 
     </Router>
+    )}
+    </Authenticator>
   );
 }
 
-function mapDispatchToProps(dispatch){
-let map = {
-  loadEmployees
+function mapDispatchToProps(dispatch) {
+  let map = {
+    loadEmployees
+  }
+  return bindActionCreators(map, dispatch);
 }
-return bindActionCreators(map,dispatch);
-}
-export default connect(null,mapDispatchToProps)(App);
+export default connect(null, mapDispatchToProps)(App);
